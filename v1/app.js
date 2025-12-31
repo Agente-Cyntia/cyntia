0285f6acb85a1038510623d2cee5cb7969559d3d
Lalita635 <lali.635@gmail.com>
2025-12-30 22:48:19 -0300

    Mas responsive

--- v1/app.js
+++ v1/app.js
@@ -136,3 +136,17 @@ if (!state.conversations || Object.keys(state.conversations).length === 0) {
 }
 
 newBtn.addEventListener("click", createConversation);
+
+const menuBtn = document.getElementById("menuBtn");
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
+
+menuBtn.addEventListener("click", () => {
+  sidebar.classList.add("open");
+  overlay.classList.add("show");
+});
+
+overlay.addEventListener("click", () => {
+  sidebar.classList.remove("open");
+  overlay.classList.remove("show");
+});
